e75e2ee23435fe2fc0521c5b744540b4c83e6821
Esther Breath <estherojile248@gmail.com>
2023-10-12 20:27:49 +0100

    test

--- test/FractionalNFT.t.sol
+++ test/FractionalNFT.t.sol
@@ -1,40 +1,50 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {FractionNFT} from "../src/FractionalNFT.sol";
-import "./Helpers.sol";
+
+import "../src/FractionalNFT.sol";
 
 contract FractionalNFT is Test{
 
    FractionalNFT fNFT;
 
+   string private _baseTokenURI;
+   uint256 private _nextTokenId = 1;
+   uint256 public fractionPrice = 0.5 ether;
+   uint256 public platformFeePercentage = 10; 
    
     uint256 totalFractions;
-     address owner;
+    address owner;
+
+    
        
-    address addrA;
-    address addrB;
-
-    uint256 privKeyA;
-    uint256 privKeyB;
-
-
-     FractionalNFT.nftDetails l;
-
       function setUp() public {
-        mPlace = new NFTMarket();
-
-        (addrA, privKeyA) = mkaddr("ADDRA");
-        (addrB, privKeyB) = mkaddr("ADDRB");
-            _nextTokenId = 1;
-            fractionPrice = 0.5 ether;
-            platformFeePercentage = 10;
-        
+       fNFT = new  FractionalNFT();
 }
 
     function testNFTGreaterThanZero() public {
     vm.expectRevert("Fractions must be greater than 0");
      FractionalNFT.tokenizeNFT(1, 0.1); 
     } 
+    function testValueNotGReaterThanFractionPrice() public {
+    vm.expectRevert("Insufficient payment for fraction");
+      fNFT.purchaseFraction(1);
+    } 
+
+  function testFractionOwnerEqualToContractOwner() public {
+     vm.expectRevert("You do not own this fraction");
+      fNFT.tranferFraction(1, "0x97C1A26482099363cb055f0F3Ca1D6057Fe55447");
+  } 
+
+    function testFractionIdEqualsAddress() public {
+    vm.expectRevert("Fraction is not available for purchase");
+      fNFT.purchaseFraction(1);
+    } 
+
+
+
+
+
+
 }
 
